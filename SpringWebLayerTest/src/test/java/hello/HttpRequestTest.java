b4f2f0a8497a20c8dfd2219443a1f012f40e521e
Rachael Baumann <baumann.rachael@gmail.com>
2018-10-02 15:24:06 -0400

    October 2 2018 PRS

---
+++ SpringWebLayerTest/src/test/java/hello/HttpRequestTest.java
@@ -0,0 +1,32 @@
+package hello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class)).contains("Hello, world!");
+		//"this" calls instance of class we're currently in
+		//"port" is an instance variable
+		//everything inside assertThat will resolve to "Hello, world!"
+		//returns true or false
+	}
+}
